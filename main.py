db4fb2edf0fa6bce092f341b8c24d3adfe4d3788
TseYeong <xieyun220823@gmail.com>
2025-04-14 15:18:32 +0800

    Main function

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+import numpy as np
+import torch
+import argparse
+from train import train
+from test import test
+
+
+def main():
+    SEED = 1234
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--phase', default='train', choices=['train', 'test'],
+                        help='Choose to train or test the model.')
+    parser.add_argument('--train_data_dir', default='./data/train/',
+                        help='Path to training data directory.')
+    parser.add_argument('--test_data_dir', default='./data/test/',
+                        help='Path to testing data directory.')
+    parser.add_argument('--model_path', default='./saved_models/resnet18.pth',
+                        help='Path to save/load the model checkpoint.')
+    parser.add_argument('--epochs', type=int, default=15, help='Number of training epochs')
+    args = parser.parse_args()
+
+    if args.phase == 'train':
+        acc = train(args.train_data_dir, args.model_path, epochs=args.epochs)
+        print(f"Train acc: {acc:.4f}")
+    else:
+        acc = test(args.test_data_dir, args.model_path)
+        print(f"Test acc: {acc:.4f}")
