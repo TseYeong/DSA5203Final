03fb0e100206c35230da040f920b093f70d1d51b
TseYeong <xieyun220823@gmail.com>
2025-04-14 17:37:24 +0800

    Data Loader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,6 +1,7 @@
 import os
 import copy
 import torch
+from tqdm import tqdm
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 
@@ -17,25 +18,10 @@ def get_train_val_loaders(data_dir, val_ratio=0.2, batch_size=32):
     Returns:
         tuple: train_loader, val_loader, custom_classes
     """
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     dataset = datasets.ImageFolder(
         root=data_dir,
         transform=transforms.ToTensor()
     )
-
-    means = torch.zeros(3).to(device)
-    stds = torch.zeros(3).to(device)
-
-    for img, _ in dataset:
-        img = img.to(device)
-        means += torch.mean(img, dim=(1, 2))
-        stds += torch.mean(img, dim=(1, 2))
-
-    means /= len(dataset)
-    stds /= len(dataset)
-    
-    means = means.cpu()
-    stds = stds.cpu()
 
     custom_classes = ['bedroom', 'Coast', 'Forest', 'Highway', 'industrial',
                       'Insidecity', 'kitchen', 'livingroom', 'Mountain', 'Office',
@@ -50,25 +36,27 @@ def get_train_val_loaders(data_dir, val_ratio=0.2, batch_size=32):
 
     train_transform = transforms.Compose([
         transforms.Grayscale(num_output_channels=3),
-        transforms.Resize(224),
+        transforms.Resize((224, 224)),
         transforms.RandomRotation(5),
         transforms.RandomHorizontalFlip(),
         transforms.RandomCrop(224, padding=4),
+        transforms.RandomApply([transforms.GaussianBlur(3)], p=0.3),
         transforms.ToTensor(),
-        transforms.Normalize(means, stds)
+        transforms.Normalize([0.5]*3, [0.5]*3)
     ])
     val_transform = transforms.Compose([
         transforms.Grayscale(num_output_channels=3),
-        transforms.Resize(224),
+        transforms.Resize((224, 224)),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(means, stds)
+        transforms.Normalize([0.5]*3, [0.5]*3)
     ])
 
-    dataset.transforms = train_transform
     val_len = int(len(dataset) * val_ratio)
     train_len = len(dataset) - val_len
     train_set, val_set = random_split(dataset, [train_len, val_len])
 
+    train_set.dataset.transform = train_transform
     val_set = copy.deepcopy(val_set)
     val_set.dataset.transform = val_transform
 
@@ -105,7 +93,7 @@ def get_test_loader(data_dir, batch_size=32):
     custom_classes = ['bedroom', 'Coast', 'Forest', 'Highway', 'industrial',
                       'Insidecity', 'kitchen', 'livingroom', 'Mountain', 'Office',
                       'OpenCountry', 'store', 'Street', 'Suburb', 'TallBuilding']
-    custom_class_to_index = {cls: i + 1 for i, cls in enumerate(custom_classes)}
+    custom_class_to_index = {cls: i for i, cls in enumerate(custom_classes)}
     test_set.class_to_idx = custom_class_to_index
     test_set.samples = [
         (path, custom_class_to_index[os.path.basename(os.path.dirname(path))])
