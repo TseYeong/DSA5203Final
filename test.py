de3f227491cbf3e3ac552ebf08c195251725a76f
TseYeong <xieyun220823@gmail.com>
2025-04-15 16:17:59 +0800

    Test the model

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from tqdm import tqdm
 from model.resnet import build_resnet
 from model.efficientnet import build_efficientnet
@@ -17,17 +18,24 @@ def test(test_dir, model_path, model, depth=18, dropout=0.0):
         model = build_resnet(len(classes), depth=depth, dropout=dropout).to(device)
 
     model.load_state_dict(torch.load(model_path, map_location=device))
+    model.to(device)
     model.eval()
 
+    criterion = nn.CrossEntropyLoss.to(device)
+
+    total_loss = 0.0
     total_acc = 0.0
 
     with torch.no_grad():
         for (images, labels) in tqdm(test_loader, desc='Testing'):
             images, labels = images.to(device), labels.to(device)
             outputs, _ = model(images)
+            loss = criterion(outputs, labels)
             acc_1, _ = calculate_topk_accuracy(outputs, labels)
             total_acc += acc_1.item()
+            total_loss += loss.item()
 
     acc = total_acc / len(test_loader)
+    loss = total_loss / len(test_loader)
 
-    return acc
+    return acc, loss
